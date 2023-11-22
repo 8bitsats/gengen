268f92ec3243888877e3661b391ea49cc490a104
8 bit <8bit@recursincribe.com>
2023-11-22 03:53:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
+
 document.addEventListener('DOMContentLoaded', () => {
-  // Assume we have color input elements with IDs 'bodyColor', 'beakColor'
-  const svgElement = document.getElementById('svg-display'); // The SVG container
+  const svgElement = document.getElementById('goose-svg'); // The SVG container
   const bodyColorInput = document.getElementById('bodyColor');
   const beakColorInput = document.getElementById('beakColor');
 
-  // Function to update SVG colors
   function updateSVGColors() {
     const bodyColor = bodyColorInput.value;
     const beakColor = beakColorInput.value;
-    // Here we would target the specific parts of the SVG and update their fill
     svgElement.querySelector('.goose-body').style.fill = bodyColor;
     svgElement.querySelector('.goose-beak').style.fill = beakColor;
   }
 
-  // Event listeners for color input changes
   bodyColorInput.addEventListener('change', updateSVGColors);
   beakColorInput.addEventListener('change', updateSVGColors);
 
-  // Function to download SVG
   function downloadSVG() {
     const svgData = svgElement.outerHTML;
     const svgBlob = new Blob([svgData], {type:"image/svg+xml;charset=utf-8"});
@@ -30,7 +26,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.removeChild(downloadLink);
   }
 
-  // Assume we have a button with ID 'downloadBtn' for downloading the SVG
   const downloadBtn = document.getElementById('downloadBtn');
   downloadBtn.addEventListener('click', downloadSVG);
 });
